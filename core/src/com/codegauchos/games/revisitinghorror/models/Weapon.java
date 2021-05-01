9e3a7a0f69265e56c845a6588a032ba9ff3a2ca7
Timothy Sandberg <tim@codegauchos.com>
2021-04-30 17:07:38 -0700

    added level property to Weapon class

--- core/src/com/codegauchos/games/revisitinghorror/models/Weapon.java
+++ core/src/com/codegauchos/games/revisitinghorror/models/Weapon.java
@@ -1,5 +1,22 @@
 package com.codegauchos.games.revisitinghorror.models;
 
 public class Weapon {
-
+	private float _strength;
+	private int _level;
+	
+	public float getStrength() {
+		return this._strength;
+	}
+	
+	public void setStrength(float value) {
+		this._strength = value;
+	}
+	
+	public int getLevel() {
+		return this._level;
+	}
+	
+	public void setLevel(int value) {
+		this._level = value;
+	}
 }
